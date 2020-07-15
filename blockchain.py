a59952e105d8a3e768ef3d422a584bf35e2cdd85
David Stout <davidgstout63@gmail.com>
2020-07-15 14:48:00 -0400

    Initial commit.

---
+++ blockchain.py
@@ -0,0 +1,13 @@
+# Creating a blockchain
+
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+    
